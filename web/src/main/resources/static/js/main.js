ea59ecb7a813901c740e30657bf85927486c097a
ChurchillWin <68127649+ChurchillWin@users.noreply.github.com>
2022-11-19 20:13:19 +0100

    Implement simple messaging with stomp

--- web/src/main/resources/static/js/main.js
+++ web/src/main/resources/static/js/main.js
@@ -1,28 +1,56 @@
 import * as StompJs from "../../../../../node_modules/@stomp/stompjs/esm6/index.js"
+let client
+const connectButton = document.getElementById("connect")
+let connected = false;
+function onConnect() {
+    client.publish({
+        destination: '/app/hello',
+        body: JSON.stringify({username: "Statham"}),
+        skipContentLengthHeader: true,
+    });
+}
 
-const client = new StompJs.Client({
-    brokerURL: 'ws://localhost:8080/hello',
+connectButton.addEventListener("click", onConnect);
 
-    debug: function (str) {
-        console.log(str);
-    },
-    reconnectDelay: 5000,
-    heartbeatIncoming: 4000,
-    heartbeatOutgoing: 4000,
-});
+function connect() {
 
-client.onConnect = function (frame) {
-    // Do something, all subscribes must be done is this callback
-    // This is needed because this will be executed after a (re)connect
-};
+    client = new StompJs.Client({
+        brokerURL: 'ws://localhost:8080/ws',
 
-client.onStompError = function (frame) {
-    // Will be invoked in case of error encountered at Broker
-    // Bad login/passcode typically will cause an error
-    // Complaint brokers will set `message` header with a brief message. Body may contain details.
-    // Compliant brokers will terminate the connection after any error
-    console.log('Broker reported error: ' + frame.headers['message']);
-    console.log('Additional details: ' + frame.body);
-};
+        connectHeaders: {
+            login: 'user',
+            passcode: 'password',
+            host: "localhost",
+            "accept-version": "v10.stomp, v11.stomp, v12.stomp"
+        },
 
-client.activate();
+        debug: function (str) {
+            console.log(str);
+        },
+        reconnectDelay: 5000,
+        heartbeatIncoming: 4000,
+        heartbeatOutgoing: 4000,
+    });
+
+    client.onConnect = function (frame) {
+        console.log("Connected")
+        console.log(frame)
+        // Do something, all subscribes must be done is this callback
+        // This is needed because this will be executed after a (re)connect
+    };
+
+    client.onStompError = function (frame) {
+        // Will be invoked in case of error encountered at Broker
+        // Bad login/passcode typically will cause an error
+        // Complaint brokers will set `message` header with a brief message. Body may contain details.
+        // Compliant brokers will terminate the connection after any error
+        console.log('Broker reported error: ' + frame.headers['message']);
+        console.log('Additional details: ' + frame.body);
+    };
+
+    client.activate();
+    client.subscribe("/topic/hello", (val) => console.log("Message:\n ", val))
+
+}
+
+connect()
